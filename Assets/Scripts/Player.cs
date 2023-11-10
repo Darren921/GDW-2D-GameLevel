1c8db22e01db1b00d0702bfaed9a15089afb5475
Darren921 <darren.chung@ontariotechu.net>
2023-11-10 14:59:01 -0500

    Fixing collision problems (walls)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,7 @@ public class Player : MonoBehaviour
     [Header("Movement")]
     private Vector2 _moveDir;
     [SerializeField] private float _moveSpeed;
+    bool _hitWall;
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +19,26 @@ public class Player : MonoBehaviour
         InputManager.EnableInGame();
 
     }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+      
+    }
 
     // Update is called once per frame
     void Update()
     {
-    //updating the position of the players
-        transform.position += (Vector3)(_moveSpeed * Time.deltaTime * _moveDir);
+        //updating the position of the players
+        if (_hitWall != true)
+        {
+            transform.position += (Vector3)(_moveSpeed * Time.deltaTime * _moveDir);
+        }
+        /*
+            if (_hitWall == true)
+        {
+            transform.position += (Vector3)(_moveDir * 0);
+        }
+         */   
+        
 
     }
     //letting the code know where the player is going next 
